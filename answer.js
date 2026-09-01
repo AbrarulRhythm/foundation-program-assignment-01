6b3e32f1035f68401452b4520aad23201df86eba
Abrarul Rhythm <abrarulrhythm19912006@gmail.com>
2026-09-02 01:54:10 +0600

    compleated question-5

--- answer.js
+++ answer.js
@@ -62,3 +62,28 @@ function getCngFare(distance, isNight = false, waitingMinutes = 0) {
 
     return minFare;
 }
+
+// question-5
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    let runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return 'Won';
+    }
+    if (ballsLeft <= 0) {
+        return 'Lost';
+    }
+
+    let requiredRate = (runsNeeded / ballsLeft) * 6;
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = 'Comfortable';
+    } else if (requiredRate > 6 && requiredRate <= 12) {
+        verdict = 'Tough';
+    } else {
+        verdict = 'Almost Impossible';
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
