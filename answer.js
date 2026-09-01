43e0f599328c65621aeb6df6142c7a660fe530f6
Abrarul Rhythm <abrarulrhythm19912006@gmail.com>
2026-09-02 01:31:39 +0600

    compleated question-4

--- answer.js
+++ answer.js
@@ -42,3 +42,23 @@ function validateUsername(userName) {
         return 'Available';
     }
 }
+
+// question-4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let minFare = 50;
+
+    if (distance > 2) {
+        let totalDistanceCharge = (distance - 2) * 15;
+        minFare = totalDistanceCharge + minFare;
+    }
+
+    if (waitingMinutes > 0) {
+        minFare += waitingMinutes * 2;
+    }
+
+    if (isNight) {
+        minFare *= 1.2;
+    }
+
+    return minFare;
+}
